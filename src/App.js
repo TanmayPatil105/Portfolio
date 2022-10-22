87b3ce47c22f8660c0ce2c2c1712642abc9ca2f7
Tanmay Patil <tanmaynpatil105@gmail.com>
2022-10-22 16:03:37 +0530

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import './App.css';
 import NavBar from '../src/components/NavBar';
-import Type from './components/Type';
-import Name from './components/Name'
-import code from './assets/code.png'
+import Body from './components/Body';
+import Learn from './components/Learn'
+import Project from './components/Project'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 
 function App() {
   
   return (
-    <div className='row'>
-      <NavBar/>
-      <div className='col-md-6'>
-        <div className="h-100 text-success d-flex align-items-center justify-content-center">
-          <Name/>
-        </div>
-        <div className='d-flex align-items-center justify-content-center' style={{marginTop:"-150px"}}>
-          <Type/>
-        </div>
-      </div>
-      <div className='col-md-5 d-flex align-items-center justify-content-center' style={{paddingTop:"150px"}}>
-          <img src={code} alt="" />
-        </div>
+    <div>
+        <Router>
+        <NavBar />
+          <Routes>
+            <Route exact path="/" element={<Body/>}/>
+            <Route exact path="/learn" element={<Learn/>}/>
+            <Route exact path="/project" element={<Project/>}/>
+          </Routes>
+    </Router>
     </div>
   );
 }
