865e1050b590fffef16356f7a5123ed11ad1e8e0
Tanmay Patil <tanmaynpatil105@gmail.com>
2022-10-22 21:28:57 +0530

    Added footer

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,8 @@
 import React, { PureComponent } from 'react'
 import Type from '../components/Type';
-import Name from '../components/Name'
-import code_lg from '../assets/code-lg.png'
-import code_sm from '../assets/code-sm.png'
+import Name from '../components/Name';
+import code_lg from '../assets/code-lg.png';
+import code_sm from '../assets/code-sm.png';
 
 class Body extends PureComponent {
     constructor() {
@@ -20,15 +20,15 @@ class Body extends PureComponent {
         return (
         <div className='row'>
             <div className='col-md-6'>
-            <div className="h-100 text-success d-flex align-items-center justify-content-center">
-            <Name size1={this.state.matches?"50px":"30px"} size2={this.state.matches?"30px":"20px"}/>
+                <div className="h-100 text-success d-flex align-items-center justify-content-center">
+                    <Name size1={this.state.matches?"50px":"30px"} size2={this.state.matches?"30px":"20px"}/>
+                </div>
+                <div className='d-flex align-items-center justify-content-center' style={{marginTop:this.state.matches?"-150px":"0px"}}>
+                    <Type size={this.state.matches?"50px":"30px"}/>
+                </div>
             </div>
-            <div className='d-flex align-items-center justify-content-center' style={{marginTop:this.state.matches?"-150px":"0px"}}>
-            <Type size={this.state.matches?"50px":"30px"}/>
-            </div>
-        </div>
-        <div className='col-md-5 d-flex align-items-center justify-content-center' style={{paddingTop:"150px"}}>
-            <img src={this.state.matches?code_lg:code_sm} alt="" />
+            <div className='col-md-5 d-flex align-items-center justify-content-center' style={{paddingTop:"150px"}}>
+                <img src={this.state.matches?code_lg:code_sm} alt="" />
             </div>
         </div>
         )
