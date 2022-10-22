be237d92aca3e96223c6f8ded7fe886eb4c226b8
Tanmay Patil <tanmaynpatil105@gmail.com>
2022-10-22 11:31:07 +0530

    added Name

---
+++ src/components/Name.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Name() {
+  return (
+    <div className="d-inline">
+      <p style={{fontSize:"50px",fontFamily:"'M PLUS Code Latin', sans-serif"}}>
+        <p style={{fontSize:"30px",fontFamily:"'M PLUS Code Latin', sans-serif",color:"#ffffff"}}>
+            Hello! 
+        </p>
+         I'm Tanmay
+      </p>
+    </div>
+  )
+}
+
+export default Name
