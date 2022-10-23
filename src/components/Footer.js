9fc6de36152ae9d7cfa6fdabcc58d3b176e0984c
Tanmay Patil <tanmaynpatil105@gmail.com>
2022-10-23 14:44:24 +0530

    fixed footer on mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React,{Component} from 'react'
 import { SocialIcon } from 'react-social-icons';
 import './Footer.css'
 
-export default function Footer() {
-  return (
-    <div className='footer row'>
-        <div className="col-md-12 d-flex align-items-center justify-content-center">
-            <SocialIcon url="https://github.com/TanmayPatil105"  bgColor='#5cb85c' fgColor='#282c34' className='mx-5'/>
-            <SocialIcon url="https://discord.com/users/Txnmxy/6057" bgColor='#5cb85c' fgColor='#282c34' className='mx-5'/>
-            <SocialIcon url="https://twitter.com/TanmayFCB" bgColor='#5cb85c' fgColor='#282c34' className='mx-5'/>
-        </div>
-    </div>
-  )
+
+class Footer extends Component{
+  constructor(props) {
+    super(props)
+    this.state = { matches: window.matchMedia("(min-width: 768px)").matches };
+  }
+  componentDidMount() {
+    const handler = e => this.setState({matches: e.matches});
+    window.matchMedia("(min-width: 768px)").addEventListener('change', handler);
+  }
+
+  render(){
+    return (
+      <div className='footer row'>
+          <div className="col-md-12 d-flex align-items-center justify-content-center">
+              <SocialIcon url="https://github.com/TanmayPatil105"  bgColor='#5cb85c' fgColor='#282c34' className={this.state.matches?'mx-5':'mx-2'}/>
+              <SocialIcon url="https://discord.com/users/Txnmxy/6057" bgColor='#5cb85c' fgColor='#282c34' className={this.state.matches?'mx-5':'mx-2'}/>
+              <SocialIcon url="https://twitter.com/TanmayFCB" bgColor='#5cb85c' fgColor='#282c34' className={this.state.matches?'mx-5':'mx-2'}/>
+          </div>
+      </div>
+    )
+  }
 }
+
+export default Footer
