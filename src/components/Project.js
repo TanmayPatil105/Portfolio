91a8f86c778984f4498d1cfed45f53ad05ce1f7e
Tanmay Patil <tanmaynpatil105@gmail.com>
2022-10-23 18:53:08 +0530

    Resized elements

--- src/components/Project.js
+++ src/components/Project.js
@@ -29,20 +29,22 @@ export default class Project extends PureComponent {
                     <Card title={"wifi-cli"} description={"Wifi through command line"} img={"https://user-images.githubusercontent.com/92677342/197331922-d6775fe4-f6fd-4d73-85d8-fa830010f6ee.png"} link={"https://github.com/TanmayPatil105/wifi-cli"} />
                 </div>
                 <div className="col-md-4 d-flex justify-content-center align-items-center" style={{marginTop:"30px"}}>
-                    <Card title={"Portfolio"} description={"My Portfolio Website"} img={"https://devstickers.com/assets/img/pro/z392.png"} link={"https://github.com/TanmayPatil105/Portfolio"} />
+                    <Card title={"Portfolio"} description={"My Portfolio Website"} img={"https://download.logo.wine/logo/React_(web_framework)/React_(web_framework)-Logo.wine.png"} link={"https://github.com/TanmayPatil105/Portfolio"} />
                 </div>
             </div>
             <div className="row">
                 <div className="col-md-4 d-flex justify-content-center align-items-center" style={{marginTop:"100px"}}>
-                    <Card title={"diff"} description={"Git diff command to compare two files line by line"} img={"https://www.kindpng.com/picc/m/403-4039227_c-language-logo-png-transparent-png.png"} link={"https://github.com/TanmayPatil105/diff"} />
+                    <Card title={"diff"} description={"Git diff command to compare two files line by line"} img={"https://user-images.githubusercontent.com/92677342/197394349-5ca03487-4d5b-4b3a-8578-90034c1a48d3.png"} link={"https://github.com/TanmayPatil105/diff"} />
                 </div>
                 <div className="col-md-4 d-flex justify-content-center align-items-center" style={{marginTop:"100px"}}>
-                    <Card title={"Resume Maker"} description={"Resume building app using JavaScript"} img={"https://1000logos.net/wp-content/uploads/2020/09/JavaScript-Logo.png"} link={"https://github.com/TanmayPatil105/Resume-Maker"} />
+                    <Card title={"Resume Maker"} description={"Resume building app using JavaScript"} img={"https://www.freepnglogos.com/uploads/javascript-png/javascript-logo-transparent-logo-javascript-images-3.png"} link={"https://github.com/TanmayPatil105/Resume-Maker"} />
                 </div>
                 <div className="col-md-4 d-flex justify-content-center align-items-center" style={{marginTop:"100px"}}>
                     <Card title={"Authentication System"} description={"QR-CODE based Identity Authentication system"} img={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwn6OFTaK4Six4Dwv-GL-4HM8vKn1N4Nfj3w&usqp=CAU"} link={"https://github.com/TanmayPatil105/qrcode-based-authentication-system"} />
                 </div>
             </div>
+            <br/>
+            <br/>
         </div>
         )
     }
