33bd4c7e8d1cfad75bcd9ec58e45102de60658f5
Tanmay Patil <tanmaynpatil105@gmail.com>
2022-10-24 13:22:24 +0530

    added images locally

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,14 @@
-import React, { PureComponent } from 'react'
-import Card from './Card'
+import React, { PureComponent } from 'react';
+import Card from './Card';
+// import logo from '../assets/languages/';
+
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => ( images[item.replace('./', '')] = r(item)));
+    return images;
+}
+
+const images = importAll(require.context('../assets/languages/', false, /\.(png|jpe?g|svg)$/));
 
 export default class Project extends PureComponent {
 
@@ -23,24 +32,24 @@ export default class Project extends PureComponent {
             </div>
             <div className='row'>
                 <div className="col-md-4 d-flex justify-content-center align-items-center" style={{marginTop:"30px"}} >
-                    <Card title={"procfetch"} description={"Linux command line utility"} img={"https://download.logo.wine/logo/C%2B%2B/C%2B%2B-Logo.wine.png"} link={"https://github.com/TanmayPatil105/procfetch"}/>
+                    <Card title={"procfetch"} description={"Linux command line utility"} img={images['c++.png']} link={"https://github.com/TanmayPatil105/procfetch"}/>
                 </div>
                 <div className="col-md-4 d-flex justify-content-center align-items-center" style={{marginTop:"30px"}}>
-                    <Card title={"wifi-cli"} description={"Wifi through command line"} img={"https://user-images.githubusercontent.com/92677342/197331922-d6775fe4-f6fd-4d73-85d8-fa830010f6ee.png"} link={"https://github.com/TanmayPatil105/wifi-cli"} />
+                    <Card title={"wifi-cli"} description={"Wifi through command line"} img={images['bash.png']} link={"https://github.com/TanmayPatil105/wifi-cli"} />
                 </div>
                 <div className="col-md-4 d-flex justify-content-center align-items-center" style={{marginTop:"30px"}}>
-                    <Card title={"Portfolio"} description={"My Portfolio Website"} img={"https://download.logo.wine/logo/React_(web_framework)/React_(web_framework)-Logo.wine.png"} link={"https://github.com/TanmayPatil105/Portfolio"} />
+                    <Card title={"Portfolio"} description={"My Portfolio Website"} img={images['react.png']} link={"https://github.com/TanmayPatil105/Portfolio"} />
                 </div>
             </div>
             <div className="row">
                 <div className="col-md-4 d-flex justify-content-center align-items-center" style={{marginTop:"100px"}}>
-                    <Card title={"diff"} description={"Git diff command to compare two files line by line"} img={"https://user-images.githubusercontent.com/92677342/197394349-5ca03487-4d5b-4b3a-8578-90034c1a48d3.png"} link={"https://github.com/TanmayPatil105/diff"} />
+                    <Card title={"diff"} description={"Git diff command to compare two files line by line"} img={images['c.png']} link={"https://github.com/TanmayPatil105/diff"} />
                 </div>
                 <div className="col-md-4 d-flex justify-content-center align-items-center" style={{marginTop:"100px"}}>
-                    <Card title={"Resume Maker"} description={"Resume building app using JavaScript"} img={"https://www.freepnglogos.com/uploads/javascript-png/javascript-logo-transparent-logo-javascript-images-3.png"} link={"https://github.com/TanmayPatil105/Resume-Maker"} />
+                    <Card title={"Resume Maker"} description={"Resume building app using JavaScript"} img={images['javascript.png']} link={"https://github.com/TanmayPatil105/Resume-Maker"} />
                 </div>
                 <div className="col-md-4 d-flex justify-content-center align-items-center" style={{marginTop:"100px"}}>
-                    <Card title={"Authentication System"} description={"QR-CODE based Identity Authentication system"} img={"https://user-images.githubusercontent.com/92677342/197455418-a41d7a88-cf57-4b70-b317-f6f6917d9e1e.png"} link={"https://github.com/TanmayPatil105/qrcode-based-authentication-system"} />
+                    <Card title={"Authentication System"} description={"QR-CODE based Identity Authentication system"} img={images['python.png']} link={"https://github.com/TanmayPatil105/qrcode-based-authentication-system"} />
                 </div>
             </div>
             <br/>
