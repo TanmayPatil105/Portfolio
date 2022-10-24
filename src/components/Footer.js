be6a0641d0207c75c9fbbab7865e1bb002f03f2b
Tanmay Patil <tanmaynpatil105@gmail.com>
2022-10-25 00:01:20 +0530

    Added hover

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,9 +17,9 @@ class Footer extends Component{
     return (
       <div className='footer row'>
           <div className="col-md-12 d-flex align-items-center justify-content-center">
-              <SocialIcon url="https://github.com/TanmayPatil105"  bgColor='#5cb85c' fgColor='#282c34' className={this.state.matches?'mx-5':'mx-2'}/>
-              <SocialIcon url="https://discord.com/users/Txnmxy/6057" bgColor='#5cb85c' fgColor='#282c34' className={this.state.matches?'mx-5':'mx-2'}/>
-              <SocialIcon url="https://twitter.com/TanmayFCB" bgColor='#5cb85c' fgColor='#282c34' className={this.state.matches?'mx-5':'mx-2'}/>
+              <SocialIcon url="https://github.com/TanmayPatil105"  bgColor='#5cb85c' fgColor='#282c34' className={this.state.matches?'mx-5 zoom-box':'mx-2 zoom-box' }/>
+              <SocialIcon url="https://discord.com/users/Txnmxy/6057" bgColor='#5cb85c' fgColor='#282c34' className={this.state.matches?'mx-5 zoom-box':'mx-2 zoom-box'}/>
+              <SocialIcon url="https://twitter.com/TanmayFCB" bgColor='#5cb85c' fgColor='#282c34' className={this.state.matches?'mx-5 zoom-box':'mx-2 zoom-box'}/>
           </div>
       </div>
     )
