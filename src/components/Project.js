d59ddd77a3bd3e6eb19e85f9efa2cf3f69075943
Tanmay Patil <tanmaynpatil105@gmail.com>
2022-11-02 01:26:48 +0530

    Fixed responsiveness

--- src/components/Project.js
+++ src/components/Project.js
@@ -24,6 +24,7 @@ export default class Project extends PureComponent {
     render() {
         return (
         <div>
+            {this.state.matches && <>
             <div className="d-flex justify-content-center align-items-center">
                 <p style={{color:"#5cb85c",fontSize:this.state.matches?"50px":"30px",fontFamily:"'M PLUS Code Latin', sans-serif",marginTop:"50px"}}>
                     My Projects
@@ -53,6 +54,38 @@ export default class Project extends PureComponent {
             </div>
             <br/>
             <br/>
+            </>}
+            {!this.state.matches && <>
+            <div className="d-flex justify-content-center align-items-center">
+                <p style={{color:"#5cb85c",fontSize:this.state.matches?"50px":"30px",fontFamily:"'M PLUS Code Latin', sans-serif",marginTop:"50px"}}>
+                    My Projects
+                </p>
+            </div>
+            <div className='row'>
+                <div className="col-md-4 d-flex justify-content-center align-items-center" style={{marginTop:"30px"}} >
+                    <Card title={"procfetch"} description={"Linux command line utility"} img={images['c++.png']} link={"https://github.com/TanmayPatil105/procfetch"}/>
+                </div>
+                <div className="col-md-4 d-flex justify-content-center align-items-center" style={{marginTop:"30px"}}>
+                    <Card title={"wifi-cli"} description={"Wifi through command line"} img={images['bash.png']} link={"https://github.com/TanmayPatil105/wifi-cli"} />
+                </div>
+                <div className="col-md-4 d-flex justify-content-center align-items-center" style={{marginTop:"30px"}}>
+                    <Card title={"Portfolio"} description={"My Portfolio Website"} img={images['react.png']} link={"https://github.com/TanmayPatil105/Portfolio"} />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-4 d-flex justify-content-center align-items-center" style={{marginTop:"30px"}}>
+                    <Card title={"diff"} description={"Git diff command to compare two files line by line"} img={images['c.png']} link={"https://github.com/TanmayPatil105/diff"} />
+                </div>
+                <div className="col-md-4 d-flex justify-content-center align-items-center" style={{marginTop:"30px"}}>
+                    <Card title={"Resume Maker"} description={"Resume building app using JavaScript"} img={images['javascript.png']} link={"https://github.com/TanmayPatil105/Resume-Maker"} />
+                </div>
+                <div className="col-md-4 d-flex justify-content-center align-items-center" style={{marginTop:"30px"}}>
+                    <Card title={"Authentication System"} description={"QR-CODE based Identity Authentication system"} img={images['python.png']} link={"https://github.com/TanmayPatil105/qrcode-based-authentication-system"} />
+                </div>
+            </div>
+            <br/>
+            <br/>
+            </>}
         </div>
         )
     }
