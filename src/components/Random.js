e0a229a4d2a3cbf582f06c4e3bc36c35639cb2c6
Tanmay Patil <tanmaynpatil105@gmail.com>
2022-12-24 20:55:56 +0530

    Changed Font

--- src/components/Random.js
+++ src/components/Random.js
@@ -14,13 +14,14 @@ $(document).ready(function(){
   animateDiv('.h');
   animateDiv('.i');
   animateDiv('.j');
+  animateDiv('.k');
+  animateDiv('.l');
 });
 
 function makeNewPosition(){
   
-  // Get viewport dimensions (remove the dimension of the div)
-  var h = $(window).height() - 50;
-  var w = $(window).width() - 50;
+  var h = $(window).height() - 100;
+  var w = $(window).width() - 100;
   
   var nh = Math.floor(Math.random() * h);
   var nw = Math.floor(Math.random() * w);
@@ -37,12 +38,14 @@ function animateDiv(myclass){
   
 };
 
+
+
 export default class Random extends PureComponent {
 
   constructor() {
     super()
     this.state = { matches: window.matchMedia("(min-width: 768px)").matches }; 
-  }
+  };
 
   componentDidMount() {
     const handler = e => this.setState({matches: e.matches});
@@ -51,19 +54,20 @@ export default class Random extends PureComponent {
 
   render() {
     return (
-        <div className="text-success" style={{fontSize:this.state.matches?"50px":"20px",fontFamily:"'M PLUS Code Latin', sans-serif",backgroundColor: 'blue'}}>
-          <div class='font-weight-bold random a text-light' style={{fontSize:this.state.matches?"45x":"20px"}}>Good luck reading things here!</div>
-          <div class='font-weight-bold random b'>Loves to Play and Watch Football</div>
-          <div class='font-weight-bold random c' style={{fontSize:this.state.matches?"35px":"15px"}}>autistic Kid</div>
-          <div class='font-weight-bold random d'>such a weirdo fr</div>
-          <div class='font-weight-bold random e' style={{fontSize:this.state.matches?"40px":"15px"}}>wannabe gopher</div>
-          <div class='font-weight-bold random f' >{"Thank you Messi for rocking my world :)"}</div>
-          <div class='font-weight-bold random g' style={{fontSize:this.state.matches?"50px":"25px"}}>likes playing online <a href="https://www.youtube.com/watch?v=DMnkwSds_RE">FPS</a> games</div>
-          <div class='font-weight-bold random h' style={{fontSize:this.state.matches?"60px":"40px"}}>obsessed with my <a href="https://github.com/TanmayPatil105">github</a></div>
-          <div class='font-weight-bold random i' style={{fontSize:this.state.matches?"50px":"30px"}}>linux is better than windows(reminder)</div>
-          <div class='font-weight-bold random j'>Addicted to <a href="https://twitter.com/TanmayFCB">twitter</a></div>
+        <div className="text-success" style={{fontSize:this.state.matches?"50px":"20px",fontFamily:"'Chakra Petch', sans-serif",backgroundColor: 'blue'}}>
+          <div class='random a text-light' style={{fontSize:this.state.matches?"45x":"20px"}}><b>Good luck reading things here!</b></div>
+          <div class='random b'><b>loves to play and watch football</b></div>
+          <div class='random c' style={{fontSize:this.state.matches?"35px":"15px"}}><b>autistic kid</b></div>
+          <div class='random d'><b>such a weirdo fr</b></div>
+          <div class='random e' style={{fontSize:this.state.matches?"40px":"15px"}}><b>wannabe gopher</b></div>
+          <div class='random f' style={{fontFamily:"'M PLUS Code Latin', sans-serif"}} ><b style={{fontFamily:"'Chakra Petch', sans-serif"}}>Thank you Messi for rocking my world</b><b> :)</b></div>
+          <div class='random g' style={{fontSize:this.state.matches?"50px":"25px"}}><b>likes playing online <a href="https://www.youtube.com/watch?v=DMnkwSds_RE">FPS</a> games</b></div>
+          <div class='random h' style={{fontSize:this.state.matches?"60px":"40px"}}><b>obsessed with my <a href="https://github.com/TanmayPatil105">github</a></b></div>
+          <div class='random i' style={{fontSize:this.state.matches?"50px":"30px"}}><b>linux is better than windows (reminder)</b></div>
+          <div class='random j'><b>Addicted to <a href="https://twitter.com/TanmayFCB">twitter</a></b></div>
+          <div class='random k' style={{fontFamily:"'M PLUS Code Latin', sans-serif"}}><b>massively misunderstood :/</b></div>
+          {/* <div class='random l'><b>Currently listening to ---</b></div> */}
         </div>
     )
   }
 }
-
